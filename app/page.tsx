2855b7c0aacebac5fe2d056bf373568445d3f063
Dmitry Shumskiy <ejedima260@gmail.com>
2023-10-25 11:19:21 +0300

    page layout update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import TestComponent from "@/components/testComponent"
 import PageLayout from "@/components/PageLayout"
 
 export default function Home() {
--- components/testComponent/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-
-
-const TestComponent:React.FC = () => {
-  return <div>Hello another world</div>
-}
-
-export default TestComponent;
