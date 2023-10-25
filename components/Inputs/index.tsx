b0182b7c0ff8cf38530b40a042137fd6629240e6
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-25 16:02:30 +0300

    Input type text : done

--- components/Inputs/index.tsx
+++ components/Inputs/index.tsx
@@ -8,7 +8,7 @@ interface InputFieldProps {
 
 const InputTypeText: React.FC<InputFieldProps> = ({ label, placeholder, name }) => {
   return (
-    <div>
+    <div className="flex flex-col items-start gap-[10px] w-full">
       {label && (
         <label className="text-white text-2xl font-normal font-medium font-normal leading-6 tracking-wider">
           {label}
@@ -18,7 +18,7 @@ const InputTypeText: React.FC<InputFieldProps> = ({ label, placeholder, name }) 
         type="text"
         placeholder={placeholder}
         name={name}
-        className="bg-black border rounded-lg border-white text-white p-2.5 max-w-[450px] w-full text-base font-normal font-medium leading-6 tracking-wider transition duration-300 ease-in-out placeholder-grey"
+        className="bg-black border rounded-lg border-white text-white p-2.5 max-w-[450px] w-full text-base font-normal font-medium leading-6 tracking-wider transition duration-300 ease-in-out placeholder-grey focus:border-main-primary-color focus:ring-0 focus:outline-none"
       />
     </div>
   );
@@ -26,7 +26,7 @@ const InputTypeText: React.FC<InputFieldProps> = ({ label, placeholder, name }) 
 
 const InputTypeTextArea: React.FC<InputFieldProps> = ({ label, placeholder, name }) => {
   return (
-    <div>
+    <div className="flex flex-col items-start gap-[10px] w-full">
       {label && (
         <label className="text-white text-2xl font-normal font-medium font-normal leading-6 tracking-wider">
           {label}
@@ -36,7 +36,7 @@ const InputTypeTextArea: React.FC<InputFieldProps> = ({ label, placeholder, name
         type="text"
         placeholder={placeholder}
         name={name}
-        className="bg-black border rounded-lg border-white text-white p-2.5 max-w-[450px] w-full text-base font-normal font-medium leading-6 tracking-wider transition duration-300 ease-in-out placeholder-grey"
+        className="bg-black border rounded-lg border-white text-white p-2.5 max-w-[450px] w-full text-base font-normal font-medium leading-6 tracking-wider transition duration-300 ease-in-out placeholder-grey focus:border-main-primary-color focus:ring-0 focus:outline-none"
       />
     </div>
   );
