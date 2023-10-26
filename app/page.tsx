f4504db4021fe6cc47aa36ca1526b7b4470dd32d
Dmitry Shumskiy <ejedima260@gmail.com>
2023-10-26 15:23:34 +0300

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { InputTypeText, SearchInput } from "@/components/Inputs";
 
 export default function Home() {
   return (
-    <PageLayout>
-      <SearchInput placeholder="Search" label="Member Fullname" name="For" />
+    <PageLayout title="Add music">
+      <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="For" />
       <InputTypeText placeholder="Name" label="Member Nickname*" name="For" />
     </PageLayout>
   );
--- components/FormLayout/index.tsx
+++ components/FormLayout/index.tsx
@@ -3,12 +3,12 @@ interface FormLayoutProps {
 }
 
 const FormLayout: React.FC<FormLayoutProps> = ({ children }) => {
-  return <form>
+  return <form className="flex flex-col gap-[45px] max-w-[550px]">
     <div>
       {children ? children : null}
     </div>
-    <div className="flex justify-between w-max">
-      <button type="submit" className="mr-[30px]">Send</button>
+    <div className="flex w-max gap-[30px]">
+      <button type="submit">Send</button>
       <button>Send later</button>
     </div>
   </form>
