de16d8b692f16ceab16a13cc082f256a4bbe7ad9
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-28 14:32:05 +0300

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon" />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
           rel="stylesheet"
