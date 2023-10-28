4c07b1270f8adbaad210cfc68c54f538d1948d92
Dmitry Shumskiy <ejedima260@gmail.com>
2023-10-28 15:47:34 +0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <PageLayout title="Add music">
-      <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="For" />
-      <InputTypeText placeholder="Name" label="Member Nickname*" name="For" />
+      <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="search" />
+      <InputTypeText placeholder="Name" label="Member Nickname*" name="nickname" />
     </PageLayout>
   );
 }
