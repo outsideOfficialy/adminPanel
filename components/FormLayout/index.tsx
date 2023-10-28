cf439e264ff2f92241e4e2d7d61ccf979be081e8
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-28 15:03:05 +0300

    formLayout fixes

--- components/FormLayout/index.tsx
+++ components/FormLayout/index.tsx
@@ -5,18 +5,23 @@ interface FormLayoutProps {
 }
 
 const FormLayout: React.FC<FormLayoutProps> = ({ children }) => {
-
   const onSendLaterClick = (e: React.FormEvent) => {
     e.preventDefault();
   };
 
-  return <form className="flex flex-col gap-[45px] max-w-[550px]">
-    {children ? children : null}
-    <div className="flex gap-[30px]">
-      <ButtonPrimary type="submit" className="bg-main-primary-color">Send</ButtonPrimary>
-      <ButtonPrimary onClick={onSendLaterClick} className="border-grey border-[1px]">Send later</ButtonPrimary>
-    </div>
-  </form>
-}
+  return (
+    <form className="flex flex-col gap-[45px] max-w-[920px]">
+      {children ? children : null}
+      <div className="flex gap-[30px]">
+        <ButtonPrimary type="submit" className="bg-main-primary-color">
+          Send
+        </ButtonPrimary>
+        <ButtonPrimary onClick={onSendLaterClick} className="border-grey border-[1px]">
+          Send later
+        </ButtonPrimary>
+      </div>
+    </form>
+  );
+};
 
 export default FormLayout;
