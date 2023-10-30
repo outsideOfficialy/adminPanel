b415fa7194780a55f237d7d3d16320b85947965a
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-31 00:02:52 +0200

    Added custom radio-btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-"use client"
+"use client";
 
 import PageLayout from "@/components/PageLayout";
-import { InputTypeText, SearchInput } from "@/components/Inputs";
-import Link from "next/link";
+import { InputTypeText, SearchInput, CustomInput, TextArea } from "@/components/Inputs";
 
 export default function Home() {
   return (
     <PageLayout title="Add music">
+      <CustomInput name="Music_type" label="Album" checked={true} />
+      <CustomInput name="Music_type" label="Single" checked={false} />
+
       <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="search" />
       <InputTypeText placeholder="Name" label="Member Nickname*" name="nickname" />
+      <TextArea placeholder="About member..." label="About member*" name="aboutMember" />
     </PageLayout>
   );
 }
--- components/Inputs/index.tsx
+++ components/Inputs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface InputFieldProps {
   label?: string;
@@ -11,6 +11,18 @@ interface SearchInputProps {
   placeholder: string;
   name: string;
   onSearch: () => void; // Функция обработчика поиска
+}
+
+interface CustomRadioButtonProps {
+  label: string;
+  name: string;
+  checked: boolean;
+}
+
+interface TextAreaFieldProps {
+  label: string;
+  placeholder: string;
+  name: string;
 }
 
 const InputTypeText: React.FC<InputFieldProps> = ({ label, placeholder, name }) => {
@@ -57,4 +69,56 @@ const SearchInput: React.FC<SearchInputProps> = ({ label, placeholder, name, onS
   );
 };
 
-export { InputTypeText, SearchInput };
+const CustomInput: React.FC<CustomRadioButtonProps> = ({ label, name, checked }) => {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  const handleLabelClick = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className="flex flex-col items-start gap-[10px] w-full max-w-450">
+      <label className="text-white text-2xl font-normal font-medium font-normal leading-6 tracking-wider">
+        {label}
+      </label>
+      <input
+        type="radio"
+        name={name}
+        checked={isChecked}
+        onChange={() => {}}
+        className="hidden focus:outline-none"
+      />
+      <label
+        htmlFor={name}
+        className={`material-symbols-outlined text-white rounded-full p-[3px] font-normal font-medium transition duration-300 ease-in-out
+          ${
+            isChecked
+              ? "border-2 border-main-primary-color bg-main-primary-color"
+              : "border-2 border-main-primary-color bg-black text-black"
+          }`}
+        onClick={handleLabelClick}
+      >
+        check
+      </label>
+    </div>
+  );
+};
+
+const TextArea: React.FC<TextAreaFieldProps> = ({ label, placeholder, name }) => {
+  return (
+    <div className="flex flex-col items-start gap-[10px] w-full">
+      {label && (
+        <label className="text-white text-2xl font-normal font-medium font-normal leading-6 tracking-wider">
+          {label}
+        </label>
+      )}
+      <textarea
+        placeholder={placeholder}
+        name={name}
+        className="bg-black min-h-[150px] border rounded-lg border-white text-white p-2.5 w-full text-base font-normal font-medium leading-normal tracking-wider transition duration-300 ease-in-out placeholder-grey focus:border-main-primary-color focus:ring-0 focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export { InputTypeText, SearchInput, CustomInput, TextArea };
