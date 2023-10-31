9f2ed01b1df1b51bd6945fc6cf90ed115cb53691
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-31 16:51:57 +0200

    radio btn ver4

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
-"use client";
-
+import React, { useState } from "react";
 import PageLayout from "@/components/PageLayout";
 import { InputTypeText, SearchInput, TextArea } from "@/components/Inputs";
-import RadioGroup from "@/components/MusicTypeSelect";
+import { RadioBtn } from "@/components/MusicTypeSelect"; // Import RadioBtn using named import
 
 export default function Home() {
+  const [type, setType] = useState("Album");
+
+  const handleChange = (event) => {
+    setType(event.target.value);
+  };
+
+  const isChecked = (value) => type === value;
+
   return (
     <PageLayout title="Add music">
-      <RadioGroup />
+      <div className="flex flex-col gap-[15px]">
+        <RadioBtn
+          id="Single"
+          name="musicType"
+          value="Single"
+          label="Single"
+          onChange={handleChange}
+          checked={isChecked("Single")}
+        />
+        <RadioBtn
+          id="Album"
+          name="musicType"
+          value="Album"
+          label="Album"
+          onChange={handleChange}
+          checked={isChecked("Album")}
+        />
+      </div>
       <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="search" />
       <InputTypeText placeholder="Name" label="Member Nickname*" name="nickname" />
       <TextArea placeholder="About member..." label="About member*" name="aboutMember" />
--- components/MusicTypeSelect/index.tsx
+++ components/MusicTypeSelect/index.tsx
@@ -1,62 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 
-interface RadioSpanProps {
-  label: string;
+interface RadioBtnProps {
+  id: string;
   name: string;
+  label: string;
+  value: string;
+  onChange: () => void;
   checked: boolean;
-  onChange: (name: string) => void;
-  selectedName: string;
 }
 
-const RadioSpan: React.FC<RadioSpanProps> = ({ label, name, checked, onChange, selectedName }) => {
-  const toggleCheck = () => {
-    onChange(name);
-  };
+// const RadioBtn: React.FC<RadioBtnProps> = ({ label, checked, onChange }) => {
+//   return (
+//     <label className={`flex gap-[10px] items-center cursor-pointer w-fit`}>
+//       <div
+//         className={`
+//           w-[24px] h-[24px] rounded-full border-2 border-main-primary-color
+//           ${checked ? "bg-main-primary-color text-white" : "bg-black text-black"}
+//           flex items-center justify-center
+//         `}
+//       >
+//         {checked && <span className="material-symbols-outlined check">check</span>}
+//       </div>
+//       <span className="text-[18px]">{label}</span>
+//       <input type="radio" className="sr-only" checked={checked} onChange={onChange} />
+//     </label>
+//   );
+// };
 
+const RadioBtn: React.FC<RadioBtnProps> = ({ id, label, name, onChange, checked, value }) => {
   return (
-    <div className="flex gap-[15px] items-center cursor-pointer w-fit" onClick={toggleCheck}>
-      <span
-        className={`text-sm material-symbols-outlined check rounded-full text-sm p-[3px] font-normal font-medium transition duration-200 ease-in-out w-fit ${
-          name === selectedName
-            ? "border-2 border-main-primary-color bg-main-primary-color text-white"
-            : "border-2 border-main-primary-color bg-black text-black"
-        }`}
-      >
-        check
-      </span>
-      <p className="text-white text-2xl font-normal font-medium font-normal leading-6 tracking-wider">
-        {label}
-      </p>
-    </div>
+    <label htmlFor={id} className="radiobutton-label">
+      <input
+        className="radiobutton-input"
+        type="radio"
+        name={name}
+        id={id}
+        value={value}
+        onChange={onChange}
+        checked={checked}
+      />
+      <span className="custom-radiobutton" />
+      {label}
+    </label>
   );
 };
 
-const RadioGroup: React.FC = () => {
-  const options = [
-    { label: "Album", name: "option1" },
-    { label: "Single", name: "option2" }
-  ];
-
-  const [selectedName, setSelectedName] = useState("");
-
-  const handleRadioChange = (name: string) => {
-    setSelectedName(name);
-  };
-
-  return (
-    <div className="flex gap-[10px] flex-col w-fit">
-      {options.map((option, index) => (
-        <RadioSpan
-          key={index}
-          label={option.label}
-          name={option.name}
-          checked={option.name === selectedName}
-          onChange={handleRadioChange}
-          selectedName={selectedName}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default RadioGroup;
+export { RadioBtn };
