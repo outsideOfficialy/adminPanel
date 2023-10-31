0ef066a40abea6a9466ae882180b09ce0e0982d0
KostyaDemchenko <kostya.doge@gmail.com>
2023-10-31 17:47:37 +0200

    radio btn var5 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,10 @@ import { RadioButton } from "@/components/RadioButtonGroup";
 export default function Home() {
   return (
     <PageLayout title="Add music">
-      <RadioButton id="Single" label="Single" name="musicType" value="Single" />
-      <RadioButton id="Album" label="Album" name="musicType" value="Album" />
+      <div className="flex flex-col gap-[10px]">
+        <RadioButton id="Single" label="Single" name="musicType" value="Single" />
+        <RadioButton id="Album" label="Album" name="musicType" value="Album" />
+      </div>
       <SearchInput onSearch={() => {}} placeholder="Search" label="Member Fullname" name="search" />
       <InputTypeText placeholder="Name" label="Member Nickname*" name="nickname" />
       <TextArea placeholder="About member..." label="About member*" name="aboutMember" />
