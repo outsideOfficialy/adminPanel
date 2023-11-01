9f9eb89be263637856953b6b2f0764307d562dd6
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-11-01 15:26:52 +0200

    add animation for confirmModal | add icons

--- components/FormLayout/index.tsx
+++ components/FormLayout/index.tsx
@@ -26,11 +26,23 @@ const FormLayout: React.FC<FormLayoutProps> = ({ children }) => {
           Send
         </ButtonPrimary>
         <ButtonPrimary onClick={onSendLaterClick} className="border-grey border-[1px]">
-          Send later
+          Send later2
         </ButtonPrimary>
       </div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      {isModalOpen && <ConfirmModal onSetModal={(value) => setModalOpen(value)}/>}
+
+
+      <div>
+        {/* shadow bg for confirmModal */}
+        <div
+          className={`${
+            isModalOpen ? "block" : "hidden"
+          } fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 z-[7]`}
+          onClick={() => setModalOpen(false)}
+          ></div>
+
+          <div className="absolute top-1/2 left-1/2 z-[10] transform -translate-x-1/2 -translate-y-1/2">
+            <ConfirmModal isOpened={isModalOpen} onSetModal={(value) => setModalOpen(value)}/>
+          </div>
       </div>
     </form>
   );
