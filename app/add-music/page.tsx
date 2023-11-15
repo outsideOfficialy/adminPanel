8740ab9bdfd279b7b0a9f578df3305810c6a3e6a
Dmitry Shumskiy <ejedima260@gmail.com>
2023-11-15 21:30:52 +0200

    s

--- app/add-music/page.tsx
+++ app/add-music/page.tsx
@@ -3,11 +3,13 @@
 import PageLayout from "@/components/PageLayout";
 import { InputTypeText, SearchInput } from "@/components/Inputs";
 import { SongsInputs } from "@/components/Inputs";
+import { RadioButton } from "@/components/RadioButtonGroup";
 
 export default function Home() {
   return (
     <PageLayout title="Add music">
       <SearchInput onSearch={() => { }} placeholder="Search" label="Music id" name="search" />
+      {/* <RadioButton id="release-type" name="album" label="Release type*"/> */}
       <InputTypeText placeholder="Name" label="Realease name*" name="nickname" />
       <SongsInputs name="songs_list[]" label="Release song(s) *"/>
     </PageLayout>
