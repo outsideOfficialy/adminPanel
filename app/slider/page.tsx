e76e6454c98372073a892318c83415de4296e612
KostyaDemchenko <kostya.doge@gmail.com>
2023-12-02 20:19:25 +0200

    Added new page "Slider"

---
+++ app/slider/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import PageLayout from "@/components/PageLayout";
+
+import { InputTypeText, SearchInput, FileInput, TextArea } from "@/components/Inputs";
+
+const Slider = () => {
+  const page = "slider";
+
+  return (
+    <PageLayout pageSubmit={page} title="Slider">
+      <SearchInput
+        pageSearch={page}
+        placeholder="ID slider..."
+        label="Slider search"
+        name="search"
+      />
+
+      <InputTypeText placeholder="Slider title...." label="Slider Title" name="title" />
+      <InputTypeText placeholder="Slider link...." label="Slider Link*" name="subtitle" />
+
+      <TextArea label="News content" placeholder="News text..." name="content" />
+
+      <FileInput
+        placeholder="Browse..."
+        accept="image/webp, image/png, image/jpg, image/jpeg"
+        name="preview_picture[]"
+        label="Slider picture mobile*"
+        id="slider-mobile-file-input"
+      />
+      <FileInput
+        placeholder="Browse..."
+        accept="image/webp, image/png, image/jpg, image/jpeg"
+        name="preview_picture[]"
+        label="Slider picture desktop*"
+        id="slider-desktop-file-input"
+      />
+    </PageLayout>
+  );
+};
+
+export default Slider;
