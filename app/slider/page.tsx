3af134950a6e187afa6f6918e351accbe4a2f40e
Dmitry Shumskiy <ejedima260@gmail.com>
2023-12-22 00:18:53 +0200

    some fixes and upgrades

--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -18,22 +18,7 @@ const Slider = () => {
       />
 
       <InputTypeText placeholder="Slider title...." label="Slider Title" name="title" />
-      <InputTypeText placeholder="Slider link...." label="Slider Link*" name="subtitle" />
-
-      {/* <FileInput
-        placeholder="Browse..."
-        accept="image/webp, image/png, image/jpg, image/jpeg"
-        name="preview_picture[]"
-        label="Slider picture mobile*"
-        id="slider-mobile-file-input"
-      />
-      <FileInput
-        placeholder="Browse..."
-        accept="image/webp, image/png, image/jpg, image/jpeg"
-        name="preview_picture[]"
-        label="Slider picture desktop*"
-        id="slider-desktop-file-input"
-      /> */}
+      <InputTypeText placeholder="Slider link...." label="Slider Link*" name="link" />
 
       <FileInput
         placeholder="Browse..."
