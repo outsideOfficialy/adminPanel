520abc832299cf6f8531e94d2d22f3c3136a9138
Dmitry Shumskiy <ejedima260@gmail.com>
2024-01-29 20:52:59 +0200

    small temp fixes

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -17,37 +17,41 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children, title, pageSubmit }) 
 
 
   if (session.status === "unauthenticated") {
-
-    window.location.href = "";
-    return <div>Login first to continue!</div>;
+    window.location.href = "/";
   }
 
   return (
     <>
-      {/* here will be menu button calling */}
-      <div className="absolute left-[10px] top-[15px] md:left-[40px] md:top-[40px]">
-        <Menu
-          list={[
-            { textContent: "Music", link: "/add-music" },
-            { textContent: "News", link: "/news" },
-            { textContent: "Merch", link: "/add-merch" },
-            { textContent: "Members", link: "/member-edit" },
-            { textContent: "Slider", link: "/slider" }
-          ]}
-        />
-      </div>
-      <div className="fixed right-8 top-8">
-        <img className="rounded-full" src={session.data?.user?.image ? session.data.user.image : ""} alt="Logo" />
-        <ButtonTemplate
-          secondary
-          onClick={() => signOut({ callbackUrl: "/" })}>
-          Signout
-        </ButtonTemplate>
-      </div>
+      {session.status === "loading" || session.status === "unauthenticated"  ? <div>
+        Loading...
+      </div> :
+        <>
+          {/* here will be menu button calling */}
+          <div className="absolute left-[10px] top-[15px] md:left-[40px] md:top-[40px]">
+            <Menu
+              list={[
+                { textContent: "Music", link: "/add-music" },
+                { textContent: "News", link: "/news" },
+                { textContent: "Merch", link: "/add-merch" },
+                { textContent: "Members", link: "/member-edit" },
+                { textContent: "Slider", link: "/slider" }
+              ]}
+            />
+          </div>
+          <div className="fixed right-8 top-8">
+            <img className="rounded-full" src={session.data?.user?.image ? session.data.user.image : ""} alt="Logo" />
+            <ButtonTemplate
+              secondary
+              onClick={() => signOut({ callbackUrl: "/" })}>
+              Signout
+            </ButtonTemplate>
+          </div>
 
-      <PageTemplate title={title}>
-        <FormLayout pageSubmit={pageSubmit}>{children ? children : null}</FormLayout>
-      </PageTemplate>
+          <PageTemplate title={title}>
+            <FormLayout pageSubmit={pageSubmit}>{children ? children : null}</FormLayout>
+          </PageTemplate>
+        </>
+      }
     </>
   );
 };
